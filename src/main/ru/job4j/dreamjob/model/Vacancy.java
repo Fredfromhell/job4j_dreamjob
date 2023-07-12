8aed9f703ac01ba37e44e3fa5b989d7071326ac0
Fred <a.bespalov@sparox.ru>
2023-07-12 11:01:07 +0300

    4. Создание вакансии [#504849]

--- src/main/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/ru/job4j/dreamjob/model/Vacancy.java
@@ -1,11 +1,15 @@
 package ru.job4j.dreamjob.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Vacancy {
 
     private int id;
@@ -16,60 +20,5 @@ public class Vacancy {
 
     private LocalDateTime creationDate;
 
-    public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.creationDate = creationDate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Vacancy vacancy = (Vacancy) o;
-        return id == vacancy.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
 }
+
